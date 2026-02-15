9043d74fa9212f92418d0ed0b42042b1bbe4edff
Najmeh Yousefi <45893022+najigit@users.noreply.github.com>
2026-02-15 20:01:18 +0330

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ with col2:
 if calculate:
     insulin= ((b+(c*40))-120)/novo
    
-   if insulin < 0:
+    if insulin < 0:
         insulin = 0
 
     # ===== گرد کردن سفارشی =====
@@ -187,6 +187,7 @@ if calculate:
         f"<h3 style='text-align:center;'>شما باید {insulin_final} واحد انسولین تزریق کنید</h3>",
         unsafe_allow_html=True
     )
+
 
 
 
